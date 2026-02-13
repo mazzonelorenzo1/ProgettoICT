d8b41a35461cc0f265c46a1da7e5969888ce920e
Lorenzo Mazzone <101272785+mazzonelorenzo1@users.noreply.github.com>
2026-02-13 18:33:16 +0100

    Update TelegramNotify.py

--- TelegramNotify.py
+++ TelegramNotify.py
@@ -4,7 +4,7 @@ import requests
 class TelegramNotifier:
     def __init__(self, token: str = None, chat_id: str = None, timeout: float = 5.0):
         self.token = token or os.getenv("8555129415:AAF8FOdqbFxlxpLPYFZ0_gFzsxArx2QT_WQ")
-        self.chat_id = chat_id or os.getenv("464019501")
+        self.chat_id = chat_id or os.getenv("-5105924827")
         self.timeout = timeout
 
         if not self.token or not self.chat_id:
